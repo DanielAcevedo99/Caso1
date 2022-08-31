73a7a1fc609cf0474d2840cc3cdd30174bf1440a
Daniel Felipe Acevedo <df.acevedo@uniandes.edu.co>
2022-08-31 16:13:45 -0500

    comm

--- Caso1/src/Main.java
+++ Caso1/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
-	
+	public static void Main (String[] args)
+	{
+		//Numero de subconjuntos
+		int n;
+		//Tamaño arreglo inicial/final
+		int a;
+		//Tamaño arreglo intermedio
+		int b;
+		
+		Scanner leer = new Scanner(System.in);
+		System.out.print("Introduzca numero de subconjuntos: ");
+		n = leer.nextInt();
+		a = leer.nextInt();
+		b = leer.nextInt();
+		
+		
+		
+	}
 }
